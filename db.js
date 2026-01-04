182ad2eb4242f84179e4c1c79d242a853e3320c8
John Week <johnweek628@gmail.com>
2026-01-04 17:59:01 +0500

    Make users DB auto-initializing

--- db.js
+++ db.js
@@ -3,11 +3,19 @@ const path = require('path');
 
 const filePath = path.join(__dirname, 'users.json');
 
+function ensureDB() {
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, JSON.stringify({}, null, 2));
+  }
+}
+
 function readDB() {
+  ensureDB();
   return JSON.parse(fs.readFileSync(filePath, 'utf8'));
 }
 
 function writeDB(data) {
+  ensureDB();
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 }
 
